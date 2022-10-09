834758a01fbfce6179c654f6810e2aebd9ceddb4
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-10-09 15:56:37 +0300

    Frontend <> Adding admin jsx component

---
+++ e_learning_frontend/src/Components/Admin.jsx
@@ -0,0 +1,14 @@
+import RightSideBar from "./LeftSideBar";
+
+function Admin() {
+  return (
+    <div className="admin">
+      <div>
+        <RightSideBar />
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Admin;
