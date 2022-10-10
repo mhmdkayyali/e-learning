b8e8266deed9eec7974cc04052d25aea346b1c69
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-10-10 22:13:19 +0300

    Frontend <> Adding App.js

--- e_learning_frontend/src/App.js
+++ e_learning_frontend/src/App.js
@@ -1,10 +1,12 @@
 import Admin from "./Components/Admin";
 import "./App.css";
+import Login from "./Components/Login";
+import Instructor from "./Components/Instructor";
 
 function App() {
   return (
     <>
-      <Admin />
+      <Instructor />
     </>
   );
 }
