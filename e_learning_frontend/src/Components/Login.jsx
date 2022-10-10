6a5b8cf036ae3efee693f8229897dbbde1c816b3
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-10-10 21:16:07 +0300

    Frontend <> Adding part of Login page

---
+++ e_learning_frontend/src/Components/Login.jsx
@@ -0,0 +1,17 @@
+import Button from "./Button";
+import Input from "./Input";
+
+const first = "Username";
+const second = "Password";
+
+function Login() {
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-header">Login</div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
