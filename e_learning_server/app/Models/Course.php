b23f5166e8a770c3e86f6342a309a48ddc876b4d
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-10-11 00:54:49 +0300

    Backend <> Adding relation in course model

--- e_learning_server/app/Models/Course.php
+++ e_learning_server/app/Models/Course.php
@@ -9,6 +9,22 @@ class Course extends Model
 {
     use HasFactory;
 
+    public function user() {
+        return $this->belongsTo(User::class, "instructor_id");
+    }
+
+    public function announcements() {
+        return $this->belongsToMany(Annoucement::class, "course_id");
+    }
+
+    public function assignments() {
+        return $this->belongsToMany(Assignment::class, "course_id");
+    }
+
+    public function registers() {
+        return $this->belongsToMany(Register::class, "course_id");
+    }
+
     protected $fillable = [
         'crn',
         'course_name',
