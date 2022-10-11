8f067605ec2e05dfbde401db4dfc6b683ce8ea75
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-10-11 23:50:59 +0300

    Backend <> Adding jwt

--- e_learning_server/app/Models/User.php
+++ e_learning_server/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
@@ -24,6 +25,10 @@ class User extends Authenticatable
         return $this->belongsToMany(Register::class, "student_id");
     }
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
     /**
      * The attributes that are mass assignable.
      *
